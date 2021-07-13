17c854b1e9c856c721fdaac442469a9419d6330f
dev-REDZONE <78139545+dev-REDZONE@users.noreply.github.com>
2021-07-14 02:09:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('ODI4OTIxNDYzMzQ4MjY0OTgy.YGwnRQ.Z27o70689tteE9FMT-IBlozKqX0');
+client.login('ODI4OTIxNDYzMzQ4MjY0OTgy.YGwnRQ.L8pFLI1uPywjAXEEgMeYBcsFpU0');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;

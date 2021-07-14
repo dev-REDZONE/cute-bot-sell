99165c8c24d865157fac0191f683a3eee36c2dd9
dev-REDZONE <78139545+dev-REDZONE@users.noreply.github.com>
2021-07-14 21:01:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -34,17 +34,6 @@ client.on("message", message => {
       "https://media.discordapp.net/attachments/786897044483604490/804760463044640808/ALANIS_MAN_GIF_99.gif",
       "https://media.discordapp.net/attachments/786897044483604490/803870704999202836/ENES_ACAR_GIF_104.gif",
       "https://media.discordapp.net/attachments/786897044483604490/803870737941135421/ENES_ACAR_GIF_15.gif"  
-      "https://media.discordapp.net/attachments/608711476219478045/864891998191878174/Tiamin_Man_Gif_120.gif"
-      "https://media.discordapp.net/attachments/608711476219478045/864891964641902602/yuzmaske.gif"            
-      "https://media.discordapp.net/attachments/608711476219478045/864883217664704532/lisamangifs_14.gif"          
-      "https://media.discordapp.net/attachments/608711476219478045/864882450701090847/saaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.gif"
-      "https://media.discordapp.net/attachments/608711476219478045/864876834713894922/image0.gif"
-      "https://media.discordapp.net/attachments/608711476219478045/864875475661881364/a_25e99596187313741dbd1923e349fc06.gif"
-      "https://media.discordapp.net/attachments/608711476219478045/864813558746054666/image0.gif"
-      "https://images-ext-2.discordapp.net/external/QgHXgR_Pdpc-7qCIcE-WpnkKFbGI95ShIdZM-S500lo/https/media.discordapp.net/attachments/786897044483604490/804760463044640808/ALANIS_MAN_GIF_99.gif"
-      "https://images-ext-2.discordapp.net/external/ENwIxM8tQ_yB3BuDBcRMwOXS7eOoUTsOtRA5mg55DVM/https/media.discordapp.net/attachments/786897044483604490/803870769313480714/Enes_Acar_GIF_70.gif"
-      "https://images-ext-2.discordapp.net/external/U2cNgUt-Oswu0JzLzB6M5DnRMBCVuuBZobG0iEXuaRc/https/media.discordapp.net/attachments/786897044483604490/804315371271749662/image0-20.gif"
-	    
     ];
 
     message.channel
@@ -1283,9 +1272,6 @@ client.on("message", message => {
       "https://media.discordapp.net/attachments/786897045436366849/804682936615829504/image0.gif",
       "https://media.discordapp.net/attachments/786897045436366849/804314115601596426/ALANIS_WOMAN_GIF_176.gif",
       "https://media.discordapp.net/attachments/786897045436366849/804280612227383316/ALANIS_WOMAN_GIF_138.gif"
-      "https://images-ext-1.discordapp.net/external/Jad-Znw-7uzeTzdFkxf6OkeWE70X1nOoNEbtBXkMpgU/https/media.discordapp.net/attachments/786897045436366849/804968223577604126/gif472.gif"
-      "https://images-ext-1.discordapp.net/external/6OiobdbI9nGzXoak42D4DdHOBL3Nga3tCMGT23-mLxg/https/media.discordapp.net/attachments/786897045436366849/804280612227383316/ALANIS_WOMAN_GIF_138.gif"
-      "https://images-ext-1.discordapp.net/external/y9wu7EcRlPN0UsiyDMjUOP5Khz70BH5f0fxdK5KPDdU/https/media.discordapp.net/attachments/786897045436366849/804314115601596426/ALANIS_WOMAN_GIF_176.gif"
     ];
 
     message.channel
@@ -1324,10 +1310,6 @@ client.on("message", message => {
       "https://media.discordapp.net/attachments/788119246517174362/804337804179275776/16-10-27-tenor.gif",
       "https://media.discordapp.net/attachments/788119246517174362/804759240451424256/Lrows_Gif_25.gif",
       "https://media.discordapp.net/attachments/788119246517174362/804759252899594259/ciftler8.gif"
-      "https://images-ext-2.discordapp.net/external/zBwBhGyDM9CtWLIRwR7xdHFfppo3nJwmkkhi1UQyTJQ/https/media.discordapp.net/attachments/608711480346542102/782233713538498600/hit_gif_5.gif"
-      "https://images-ext-2.discordapp.net/external/-3sNdb7M3XUVmWsYYEvyxOvfPQlQRLsums1WlJtgz8M/https/media.discordapp.net/attachments/788119246517174362/804188805204410378/2.gif"
-      "https://images-ext-1.discordapp.net/external/HladBOD-vU9wTTKpKAhtnVZbmcr4nq7uvHlFvXnEVz8/https/media.discordapp.net/attachments/788119246517174362/804337804179275776/16-10-27-tenor.gif"
-      "https://images-ext-2.discordapp.net/external/AQz9TCzlqtPJWM33yGrgpOx5Se1GA-eORwOhfu3CJLI/https/media.discordapp.net/attachments/608711480346542102/782196955488321556/a_637b8e2042d540a1e5e28282e3fe5cc7.gif"
     ];
 
     message.channel

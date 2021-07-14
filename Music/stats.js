873ac4b90ea2d71b0329560b8fdfbf95cf33faac
dev-REDZONE <78139545+dev-REDZONE@users.noreply.github.com>
2021-07-14 11:58:42 +0300

    Update stats.js

--- Music/stats.js
+++ Music/stats.js
@@ -10,26 +10,24 @@ module.exports = {
   name: "stats",
   aliases: ["stats"],
   cooldown: 8,
-  description: "** Invite pore bot**",
+  description: "** Invite Calms,bot**",
   execute(message) {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
     .setThumbnail(``)
-    .setImage(`https://media.discordapp.net/attachments/848815969233403934/850660018532777984/emoji_17.gif `)
+    .setImage(`https://cdn.discordapp.com/emojis/827810082850340885.gif`)
     .setTitle(`CAlms is One`)
     .setDescription(`
-<a:emoji_17:850611627321327617> OWNER BOT <@743887896481628190> <a:emoji_1:848805449155215391>
+<a:emoji_17:850611627321327617> OWNER BOT <@767108799785598977> <a:emoji_1:848805449155215391>
 <a:emoji_16:850611586921398283>
-<a:emoji_29:850653075055050772>[YOUTUBE](https://youtube.com/channel/UCU7jT6CLQvKIkyEzOuJFUPQ)
+<:g_:850633528697159710> [GITHUP](https://github.com/dev-REDZONE)
 <a:emoji_16:850611586921398283>
-<:g_:850633528697159710> [GITHUP](https://github.com/MAPXOR12)
+[SERVER SUPPORT](https://discord.gg/vWKsHCNQce)
 <a:emoji_16:850611586921398283>
-[SERVER SUPPORT](https://discord.gg/TTuqRTQrse)
+[SPONCAR SERVER](https://discord.gg/jokerstaff)
 <a:emoji_16:850611586921398283>
-[SPONCAR SERVER](https://discord.gg/EpBZZYFre6)
-<a:emoji_16:850611586921398283>
-[INVITEBOT](https://discord.com/oauth2/authorize?client_id=828921463348264982&permissions=70282305&scope=bot)`)
+[INVITEBOT](https://discord.com/api/oauth2/authorize?client_id=828921463348264982&permissions=2184576832&scope=bot)`)
 
    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
    .setColor("RANDOM")

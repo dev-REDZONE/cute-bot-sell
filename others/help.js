82a577d6d6706ab580b90fd57039edfcb89a474f
dev-REDZONE <78139545+dev-REDZONE@users.noreply.github.com>
2021-07-21 16:18:04 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -65,7 +65,7 @@ queue ・ filter ・ radio・loop
 **Links**
 [SUPPORT](https://discord.gg/jokerstaff)       [INVITE](https://discord.com/api/oauth2/authorize?client_id=828921463348264982&permissions=2184576832&scope=bot)
 `)
-    **Top.gg Link**
+    Top.gg Links
       [Website] (https://top.gg/bot/828921463348264982)- [Vote] (https://top.gg/bot/828921463348264982/vote)
 
    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))

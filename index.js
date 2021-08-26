a80c2f2bca50b651d2ba82f4eb0a911a19ad999b
dev-REDZONE <78139545+dev-REDZONE@users.noreply.github.com>
2021-08-26 21:48:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('lera token dane By RedZone');
+client.login('ODI4OTIxNDYzMzQ4MjY0OTgy.YGwnRQ.Kh1FykI8DefVRlU6uMwVAZOabS4');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
